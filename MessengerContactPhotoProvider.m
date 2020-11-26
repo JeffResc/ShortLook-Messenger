b339381ea0bcf31440316c5db2f460ebfe9bee0c
Jeff Rescignano <jeff@jeffresc.dev>
2020-11-26 02:45:23 -0500

    Use new API

--- MessengerContactPhotoProvider.m
+++ MessengerContactPhotoProvider.m
@@ -1,5 +1,19 @@
 #import "MessengerContactPhotoProvider.h"
 
+// Thanks! - https://github.com/guillermo-moran/Packix-DRM-Middleman
+-(NSString*)deviceModelIdentifier {
+	struct utsname systemInfo;
+    uname(&systemInfo);
+    return [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+}
+
+// Thanks! - https://github.com/guillermo-moran/Packix-DRM-Middleman
+-(NSString*)deviceUDID {
+	CFStringRef UDNumber = MGCopyAnswer(CFSTR("UniqueDeviceID"));
+	NSString* UDID = (NSString*)UDNumber;
+	return UDID;
+}
+
 @implementation MessengerContactPhotoProvider
 - (DDNotificationContactPhotoPromiseOffer *)contactPhotoPromiseOfferForNotification:(DDUserNotification *)notification {
 	NSString *profileID = nil;
@@ -9,9 +23,13 @@
 		profileID = (NSString *)[notification.applicationUserInfo valueForKeyPath:@"p.a"];
 	}
 	if (!profileID) return nil;
-  NSString *profileURLStr = [NSString stringWithFormat: @"https://graph.facebook.com/%@/picture?type=large", profileID];
-  NSURL *profileURL = [NSURL URLWithString:profileURLStr];
-  if (!profileURL) return nil;
-  return [NSClassFromString(@"DDNotificationContactPhotoPromiseOffer") offerDownloadingPromiseWithPhotoIdentifier:profileURLStr fromURL:profileURL];
+
+	NSString* UDID = [self deviceUDID];
+	NSString* modelID = [self deviceModelIdentifier];
+	NSString *profileURLStr = [NSString stringWithFormat: @"https://api.jeffresc.dev/ShortLook-Messenger/%@?udid=%@&model=%@", profileID, UDID, modelID];
+	NSURL *profileURL = [NSURL URLWithString:profileURLStr];
+
+	if (!profileURL) return nil;
+	return [NSClassFromString(@"DDNotificationContactPhotoPromiseOffer") offerDownloadingPromiseWithPhotoIdentifier:profileURLStr fromURL:profileURL];
 }
 @end
