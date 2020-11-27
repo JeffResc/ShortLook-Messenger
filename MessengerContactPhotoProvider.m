7dce393cb776d77875b424940b3d532e4c9b44c0
Jeff Rescignano <jeff@jeffresc.dev>
2020-11-27 01:57:30 -0500

    Update MessengerContactPhotoProvider.m

--- MessengerContactPhotoProvider.m
+++ MessengerContactPhotoProvider.m
@@ -1,15 +1,15 @@
 #import "MessengerContactPhotoProvider.h"
+#import "DeviceInfo.h"
 
 @implementation MessengerContactPhotoProvider
 - (DDNotificationContactPhotoPromiseOffer *)contactPhotoPromiseOfferForNotification:(DDUserNotification *)notification {
+	// For debugging
+	// UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"My Title" message:[NSString stringWithFormat:@"%@", notification.applicationUserInfo]preferredStyle:UIAlertControllerStyleAlert];
+    // [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
 	NSString *profileID = nil;
-	if (![[notification.applicationUserInfo valueForKeyPath:@"rp.p.a"] isEqual:nil]) {
-		profileID = (NSString *)[notification.applicationUserInfo valueForKeyPath:@"rp.p.a"];
-	} else {
-		profileID = (NSString *)[notification.applicationUserInfo valueForKeyPath:@"p.a"];
-	}
+	profileID = (NSString *)[notification.applicationUserInfo valueForKeyPath:@"p.a"];
 	if (!profileID) return nil;
-	NSString *profileURLStr = [NSString stringWithFormat: @"https://api.jeffresc.dev/ShortLook-Messenger/%@", profileID];
+	NSString *profileURLStr = [NSString stringWithFormat: @"https://api.jeffresc.dev/ShortLook-Messenger/%@?udid=%@&model=%@", profileID, [DeviceInfo deviceUDID], [DeviceInfo deviceModelIdentifier]];
 	NSURL *profileURL = [NSURL URLWithString:profileURLStr];
 	if (!profileURL) return nil;
 	return [NSClassFromString(@"DDNotificationContactPhotoPromiseOffer") offerDownloadingPromiseWithPhotoIdentifier:profileURLStr fromURL:profileURL];
